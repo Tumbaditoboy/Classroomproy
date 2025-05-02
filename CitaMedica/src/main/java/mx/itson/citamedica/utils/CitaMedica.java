1aca0554628b56abb9080f72f2862fe464e53c78
Akane <Akane@DESKTOP-GLP9P0S>
2025-05-01 19:17:45 -0700

    Adición del archivo Docker y configuración en POM para que el archivo .jar en su configuracion detecte un archivo main. Adición de una clase main para que se corra al momento de iniciar el programa.

---
+++ CitaMedica/src/main/java/mx/itson/citamedica/utils/CitaMedica.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.citamedica.utils;
+
+/**
+ *
+ * @author Akane
+ */
+public class CitaMedica {
+    public static void main(String[] args) {
+        System.out.println("Ya llegarom losh dokersh");
+        String url = "https://calendly.com/dzlann13x";
+        System.out.println("Abre esta URL en tu navegador: " + url);
+    }
+}
